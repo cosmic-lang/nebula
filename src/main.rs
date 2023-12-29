d1b896f0c482196da8a5e692baf5f642b9bc367c
dwclake <clake.dw@gmail.com>
2023-12-29 09:15:41 -0800

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use anyhow::Result;
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
-#[command(name="tea", author="rexlang", version, about, arg_required_else_help = true)]
+#[command(name="nebula", author="cosmic-lang", version, about, arg_required_else_help = true)]
 /// Package Manager for Rex
 struct Cli {
   #[command(subcommand)]
